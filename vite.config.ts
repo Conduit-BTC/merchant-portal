c88ef08eb718b637668ceef3c454b20580fa02ec
aceaspades <aceaspades@protonmail.com>
2025-05-28 21:31:11 -0700

    Dockerfile, Caddyfile, nostr-commerce-schema as submodule

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import path, { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [preact(), tailwindcss(),
-  ],
+  plugins: [preact(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@root': path.resolve(__dirname, '.')
-    }
-  }
+      "@": path.resolve(__dirname, "src"),
+      "@root": path.resolve(__dirname, "."),
+      "nostr-commerce-schema": resolve(
+        __dirname,
+        "external/nostr-commerce-schema"
+      ),
+    },
+  },
 });
